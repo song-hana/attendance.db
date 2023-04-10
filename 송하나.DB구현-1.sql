3d2d4995294c31f9f939dfb3a02f0c92cf37f892
song1 <songhana369@gmail.com>
2023-04-10 14:03:36 +0900

    '0410'

--- 송하나.DB구현-1.sql
+++ 송하나.DB구현-1.sql
@@ -147,11 +147,43 @@ create sequence attendance.plan_planno_seq
 -- companys
 insert into attendance.companys values('company', 'company', '하나웍스', 1234567891, '서울시 영등포구 신풍로 101',
 '601호', 12554, '0228465678', 'java123@naver.com','김소피아');
+insert into attendance.companys values('company2', 'company2', '하나제약', 1234554321, '서울시 관악구 신림로 201',
+'401호', 12555, '0228465688', 'java123@naver.com','마르소');
 
 -- employees
 insert into attendance.employees values(attendance.emp_empno_seq.nextval, 'user', 'user',
     '송하나', '서울시 관악구 신림로 340', '601호', 34502, '01012345678', 'java123@naver.com',
-    '2023-03-29', '팀장', '9510012011111', null, 'company');
+    '2022-04-01', '팀장', '9510012011111', null, 'company');
+insert into attendance.employees values(attendance.emp_empno_seq.nextval, 'user2', 'user2',
+    '김민형', '서울시 영등포구 신풍로 340', '601호', 34503, '01038521234', 'java22@naver.com',
+    '2022-12-05', '사원', '9709161022222', null, 'company');
+insert into attendance.employees values(attendance.emp_empno_seq.nextval, 'user3', 'user3',
+    '최서영', '서울시 관악구 신림로 102', '301호', 34504, '01018754315', 'java13@naver.com',
+    '2020-03-29', '주임', '9707212033333', null, 'company');
+insert into attendance.employees values(attendance.emp_empno_seq.nextval, 'user4', 'user4',
+    '이선재', '서울시 관악구 신림로 103', '103호', 34505, '01056891245', 'java44@naver.com',
+    '2010-03-01', '과장', '9211051055555', null, 'company2');
+insert into attendance.employees values(attendance.emp_empno_seq.nextval, 'user5', 'user5',
+    '오진성', '서울시 관악구 신림로 104', '104호', 34506, '01045678451', 'java55@naver.com',
+    '2005-03-01', '부장', '9303021066666', null, 'company2');
+
+-- plans
+insert into attendance.plans values(attendance.plan_planno_seq.nextval, '회식', '2023-04-03', '이젠 삼겹살 16:00', 'company');
+insert into attendance.plans values(attendance.plan_planno_seq.nextval, '산악회', '2023-04-16', '관악산', 'company2');
+
+-- holidays
+
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-03-27', '개인사유', 2);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-04-08', '개인사유', 4);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-04-14', '개인사유', 5);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-04-16', '개인사유', 5);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-04-20', '개인사유', 3);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-04-27', '개인사유', 2);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-05-04', '개인사유', 3);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-05-10', '개인사유', 4);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-05-14', '개인사유', 5);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-05-16', '개인사유', 5);
+insert into attendance.holidays values(attendance.hol_holno_seq.nextval, '2023-05-20', '개인사유', 3);
 
 -- admins
 insert into attendance.admins values('admin', 'admin');
